838e9c315a87a3203fc7045b948bbb32cc26a15e
Corly <dragos.corlatescu@gmail.com>
2014-02-15 19:48:38 +0200

    Write buttons' events in Start Activity

--- Mima/src/com/mima/StartingActivity.java
+++ Mima/src/com/mima/StartingActivity.java
@@ -1,15 +1,39 @@
 package com.mima;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class StartingActivity extends Activity {
-
+	private Button allTogether;
+	private Button vsTeam;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_starting);
+		
+		allTogether = (Button) findViewById(R.id.image_button_all_together);
+		vsTeam = (Button) findViewById(R.id.image_button_team_vs_team);
+		
+		allTogether.setOnClickListener(new OnClickListener() {
+		
+			@Override
+			public void onClick(View v) {
+				//start activity for all together
+			}
+		});
+
+		vsTeam.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//start activity for team vs team
+			}
+		});
 	}
 
 	@Override
