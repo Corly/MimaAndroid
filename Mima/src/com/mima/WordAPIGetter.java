1ec307628d854bf92064798147d044a7372294bf
Corly <dragos.corlatescu@gmail.com>
2014-02-16 18:23:16 +0200

    Create class for getting the word from the server.

---
+++ Mima/src/com/mima/WordAPIGetter.java
@@ -0,0 +1,34 @@
+package com.mima;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class WordAPIGetter {
+	private String URL = "mima.andreiduma.ro/api/";
+	
+	public String getWord() throws ClientProtocolException, IOException {
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpResponse response = httpClient.execute(new HttpGet(URL));
+		StatusLine statusLine = response.getStatusLine();
+		
+		if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+	        response.getEntity().writeTo(out);
+	        out.close();
+	        String responseString = out.toString();
+	        return responseString;
+		} else{
+	        //Closes the connection.
+	        response.getEntity().getContent().close();
+	        throw new IOException(statusLine.getReasonPhrase());
+	    }
+	}
+}
